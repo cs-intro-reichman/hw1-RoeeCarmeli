b72c0f5bd6c7427f9753742a65f35b50c2bc8877
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-07 18:25:03 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -2,10 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 	double startFunds = Double.parseDouble(args[0]);
-	double intrest = Double.parseDouble(args[1]) / 100;
+	double intrest = Double.parseDouble(args[1]);
+	double newIntrest = intrest / 100;
 	int years = Integer.parseInt(args[2]);
-	double futureValue = startFunds * (Math.pow((1 + intrest), years));
-	System.out.println((int) futureValue);
+	double futureValue = startFunds * (Math.pow((1 + newIntrest), years));
+	System.out.println("After " + years + " years, a $" + (int)startFunds + " " + "saved at $" + intrest + " will yield $" + ((int) futureValue));
 	//trail..
 	}
 }
