f8ea1542f34a323b9fa75edbe1e830698921ee2e
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-07 17:55:30 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,10 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+	double startFunds = Double.parseDouble(args[0]);
+	double intrest = Double.parseDouble(args[1]) / 100;
+	int years = Integer.parseInt(args[2]);
+	double futureValue = startFunds * (Math.pow((1 + intrest), years));
+	System.out.println((int) futureValue);
 	}
 }
