58d7679310fde9568dc9f9a1d2292de26c4803ed
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-07 17:55:33 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		double totalBill = Double.parseDouble(args[3]);
 		double splitBill = totalBill / 3.0;
 		double newBill = Math.ceil(splitBill);
-		System.out.println("dear " + name1 + " " + name2 + " and" + name3 + ": pay " + newBill + " Shekels each");
+		System.out.println("dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + newBill + " Shekels each");
 	}
 }
