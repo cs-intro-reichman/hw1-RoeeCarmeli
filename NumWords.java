9409dcb20b29d54b1ef3cace5bc3436631aa81e7
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-07 21:13:38 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,6 @@
 public class NumWords {
 	public static void main(String args[]) {
 		int fullNum = Integer.parseInt(args[0]);
-		fullNum = fullNum % 1000;
 		int hundreds = fullNum / 100;
 		fullNum = fullNum % 100;
 		int tens = fullNum / 10;
