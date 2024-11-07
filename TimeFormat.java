0dfa8a33ed33052c1ed0cd8d3e1a2a14b8a8792d
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2024-11-07 22:20:57 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,15 +3,31 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		if (hours > 12)
-			if (minutes > 9) System.out.println((hours - 12) + " : " + minutes + " PM");
-			else System.out.println((hours - 12) + " : 0" + minutes + " PM");
-		if (hours <= 12);
-			if (hours == 0)			
+		if (hours >= 12)
+		{
+			if (hours == 12) 
+			{
+				if (minutes > 9) System.out.println((hours) + " : " + minutes + " PM");
+			else System.out.println((hours) + " : 0" + minutes + " PM");
+			}
+			else
+			{
+				if (minutes > 9) System.out.println((hours - 12) + " : " + minutes + " PM");
+				else System.out.println((hours - 12) + " : 0" + minutes + " PM");
+			}
+		}
+		else
+		{
+			if (hours == 00)
+			{			
 				if (minutes > 9) System.out.println((hours) + "0 : " + minutes + " AM");
 				else System.out.println((hours) + "0 : 0" + minutes + " AM");
+			}
 			else
-			if (minutes > 9) System.out.println((hours) + " : " + minutes + " AM");
-			else System.out.println((hours) + " : 0" + minutes + " AM");
+			{
+				if (minutes > 9) System.out.println((hours) + " : " + minutes + " AM");
+				else System.out.println((hours) + " : 0" + minutes + " AM");
+			}
+		}
 	}
 }
